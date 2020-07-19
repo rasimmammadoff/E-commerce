e08fca8d02ab2bd5768aec4e9fce2359ea7857a0
Rasim <rasimmammadoff1@gmail.com>
2020-07-19 22:54:31 +0400

    Show image in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Product
 
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id','title','amount','price','image']
+    list_display = ['id','title','amount','price','image_field','image']
     list_filter = ['amount','price']
+
+    def image_field(self,obj):
+        return format_html('<img src="{}"  width="40px" height="40px"/>'.format(obj.image.url))
+    image_field.short_description = 'Image'
 
 admin.site.register(Product,ProductAdmin)
