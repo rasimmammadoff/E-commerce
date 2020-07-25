431385bdbb22ebff648fc00abdca705d22906bf0
Rasim <rasimmammadoff1@gmail.com>
2020-07-25 15:04:59 +0400

    Category table and category admin added

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import Product
+from .models import Product,Category
 
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id','title','amount','price','image_field','image']
+    list_display = ['id','title','amount','price','image_field','created_at','updated_at']
     list_filter = ['amount','price']
+    readonly_fields = ['created_at','updated_at']
 
     def image_field(self,obj):
         return format_html('<img src="{}"  width="40px" height="40px"/>'.format(obj.image.url))
     image_field.short_description = 'Image'
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','title','created_at','updated_at']
+    list_filter = ['title']
+    readonly_fields = ['created_at','updated_at']
+
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Category,CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -8,6 +8,19 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2,max_digits=4)
     minSize = models.IntegerField(default=21)
     maxSize = models.IntegerField(default=26)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category',null=True,blank=True,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
